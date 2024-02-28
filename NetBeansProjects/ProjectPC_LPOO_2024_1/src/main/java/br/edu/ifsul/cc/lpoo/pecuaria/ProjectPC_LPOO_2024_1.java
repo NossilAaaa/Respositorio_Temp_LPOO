94d1a8894649edbf230b3f6acee1c97b242d836e
ALisson <126485394+NossilAaaa@users.noreply.github.com>
2024-02-28 11:42:24 -0300

    Add files via upload

---
+++ NetBeansProjects/ProjectPC_LPOO_2024_1/src/main/java/br/edu/ifsul/cc/lpoo/pecuaria/ProjectPC_LPOO_2024_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.pecuaria;
+
+/**
+ *
+ * @author 20212PF.CC0020
+ */
+public class ProjectPC_LPOO_2024_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
