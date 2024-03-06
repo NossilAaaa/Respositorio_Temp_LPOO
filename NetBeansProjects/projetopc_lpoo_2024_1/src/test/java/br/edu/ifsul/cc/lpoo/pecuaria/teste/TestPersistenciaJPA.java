b51ad1a718fb3d436503020bec72af75285dae5c
ALisson <126485394+NossilAaaa@users.noreply.github.com>
2024-03-06 11:43:28 -0300

    Aula 06/03/2024 - JPA testes _ parte 2

--- NetBeansProjects/projetopc_lpoo_2024_1/src/test/java/br/edu/ifsul/cc/lpoo/pecuaria/teste/TestPersistenciaJPA.java
+++ NetBeansProjects/projetopc_lpoo_2024_1/src/test/java/br/edu/ifsul/cc/lpoo/pecuaria/teste/TestPersistenciaJPA.java
@@ -4,6 +4,7 @@
  */
 package br.edu.ifsul.cc.lpoo.pecuaria.teste;
 
+import br.edu.ifsul.cc.lpoo.pecuaria.modelo.Raca;
 import br.edu.ifsul.cc.lpoo.pecuaria.modelo.dao.PersistenciaJPA;
 import org.junit.Test;
 
@@ -12,7 +13,8 @@ import org.junit.Test;
  * @author 20212PF.CC0020
  */
 public class TestPersistenciaJPA {
-    
+
+    /*  
     @Test
     public void testConexaoJPA(){
         //criar um objeto do tipo PersistenciaJPA.
@@ -25,5 +27,67 @@ public class TestPersistenciaJPA {
                         
         }
     }
+
+    @Test
+    public void GerarRacas() throws Exception {
+        PersistenciaJPA jpa = new PersistenciaJPA();
+        if (jpa.conexaoAberta()) {
+            System.out.println("testInsercaoRacaJPA: conectou no BD via jpa...");
+
+            Raca r = new Raca();
+            r.setNome("Raca de teste!!!");
+
+            jpa.persist(r);
+            System.out.println("Raca id: " + r.getId());
+
+            jpa.fecharConexao();
+        } else {
+            System.out.println("testInsercaoRacaJPA: não conectou no BD...");
+        }
+
+    }
+    @Test
+    public void AlteraRaca() throws Exception {
+        PersistenciaJPA jpa = new PersistenciaJPA();
+
+        if (jpa.conexaoAberta()) {
+
+            System.out.println("Conectou ao BD via JPA!");
+
+            Raca r = (Raca) jpa.find(Raca.class, Integer.valueOf(1));
+            if (r != null) {
+                    r.setNome("Nome Alterado");
+                    jpa.persist(r);
+            } else {
+                System.out.println(" Não encotrou a raca=1 no BD...");
+            }
+
+            jpa.fecharConexao();
+        } else {
+            System.out.println("testInsercaoRacaJPA: não conectou no BD...");
+        }
+    }*/
     
+    
+    @Test
+    public void RemoveRaca() throws Exception {
+        PersistenciaJPA jpa = new PersistenciaJPA();
+
+        if (jpa.conexaoAberta()) {
+
+            System.out.println("Conectou ao BD via JPA!");
+
+            Raca r = (Raca) jpa.find(Raca.class, Integer.valueOf(1));
+            if (r != null) {
+                    jpa.remover(r);
+                    System.out.println("Raca Removida id: "+r.getId());
+            } else {
+                System.out.println(" Não encotrou a raca=1 no BD...");
+            }
+
+            jpa.fecharConexao();
+        } else {
+            System.out.println("testInsercaoRacaJPA: não conectou no BD...");
+        }
+    }
 }
