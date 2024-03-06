c66c4a5cb21fc43ab3ee9f9479d5abe64b59fd06
ALisson <126485394+NossilAaaa@users.noreply.github.com>
2024-03-06 10:58:16 -0300

    Aula 06/03/2024 - Parte 1 - JPA testes

---
+++ NetBeansProjects/projetopc_lpoo_2024_1/src/test/java/br/edu/ifsul/cc/lpoo/pecuaria/teste/TestPersistenciaJPA.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.pecuaria.teste;
+
+import br.edu.ifsul.cc.lpoo.pecuaria.modelo.dao.PersistenciaJPA;
+import org.junit.Test;
+
+/**
+ *
+ * @author 20212PF.CC0020
+ */
+public class TestPersistenciaJPA {
+    
+    @Test
+    public void testConexaoJPA(){
+        //criar um objeto do tipo PersistenciaJPA.
+        PersistenciaJPA jpa = new PersistenciaJPA();
+        if(jpa.conexaoAberta()){
+            System.out.println("conectou no BD via jpa ...");
+            jpa.fecharConexao();
+        }else{
+            System.out.println("nao conectou no BD ...");
+                        
+        }
+    }
+    
+}
